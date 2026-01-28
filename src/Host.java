5ef38b66b5911867e40a515c9afcf3e9708c27e2
KSanders04 <kasanders2004@gmail.com>
2026-01-28 11:15:11 -0500

    Add start reciever function to host file. Also add hardcoded config for testing purposes.

--- src/Host.java
+++ src/Host.java
@@ -1,95 +1,61 @@
-import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
 public class Host {
 
-    private String myID;
-    private ConfigParser.DeviceInfo myInfo;
-    private ConfigParser.DeviceInfo switchInfo;
+    private String hostId;
+    private String hostIp;
+    private int hostPort;
+
+    private String switchIp;
+    private int switchPort;
 
     private DatagramSocket socket;
 
-    public Host(String id, String configFile) throws Exception {
-        this.myID = id;
-
-        ConfigParser parser = new ConfigParser(configFile);
-        myInfo = parser.getDevice(id);
-
-        if (myInfo == null) {
-            throw new RuntimeException("Host ID not found in config file.");
+    public void config() {
+        switchIp = "127.0.0.1";
+        int port = 3000;
+        switch (hostId) {
+            case "A" :
+                hostIp = "127.0.0.1";
+                hostPort = 5001;
+                break;
+            case "B" :
+                hostIp = "127.0.0.1";
+                hostPort = 5002;
+                break;
+            case "C":
+                hostIp = "127.0.0.1";
+                hostPort = 5003;
+                break;
+            case "D":
+                hostIp = "127.0.0.1";
+                hostPort = 5004;
+                break;
         }
-
-
-        String switchID = myInfo.neighbors.get(0);
-        switchInfo = parser.getDevice(switchID);
-
-        socket = new DatagramSocket(myInfo.port);
-
-        System.out.println("Host " + myID + " started on port " + myInfo.port);
-        System.out.println("Connected to switch " + switchID + " at " + switchInfo.ip + ":" + switchInfo.port);
-
-        startReceiverThread();
     }
-
-    private void startReceiverThread() {
-        Thread t = new Thread(() -> {
-            byte[] buf = new byte[2048];
-
-            while (true) {
-                try {
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+    public void startReciever() {
+        Thread receiver = new Thread(() -> {
+            try {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                     socket.receive(packet);
-
-                    String raw = new String(packet.getData(), 0, packet.getLength());
-                    Frame f = Frame.parse(raw);
-
-                    if (!f.dst.equals(myID)) {
-                        System.out.println("[DEBUG] Received flooded frame not meant for me. SRC=" + f.src);
-                    } else {
-                        System.out.println("Message from " + f.src + ": " + f.msg);
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    String frame = new String(packet.getData(), 0, packet.getLength());
+                    handleFrame(frame);
                 }
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         });
-
-        t.setDaemon(true);
-        t.start();
+        receiver.setDaemon(true);
+        receiver.start();
     }
 
-    public void sendMessage(String dst, String msg) throws IOException {
-        Frame f = new Frame(myID, dst, msg);
-        String raw = f.serialize();
+    private void handleFrame(String frame) {
 
-        byte[] data = raw.getBytes();
-        DatagramPacket packet = new DatagramPacket(
-                data,
-                data.length,
-                InetAddress.getByName(switchInfo.ip),
-                switchInfo.port
-        );
-
-        socket.send(packet);
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.out.println("Usage: java Host <ID> <config-file>");
-            return;
-        }
-
-        Host host = new Host(args[0], args[1]);
-
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.print("Enter destination and message (e.g., D hello): ");
-            String dst = sc.next();
-            String msg = sc.nextLine().trim();
-
-            host.sendMessage(dst, msg);
-        }
     }
 }
